32368920d5e4c5e26d6bc9beff4708fe1b57963b
İbrahim Turan <ibrahimturan002@gmail.com>
2021-07-08 00:58:50 +0300

    add money function

--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -3,7 +3,22 @@ const getLocaleFromPath = () => window
 
 const isLocaleValid = (locales, locale) => locales.includes(locale);
 
+/*
+* Formats numbers as money, if the locale is missing browser default will be used.
+* @param args default { locale: String, currency: String, amount: Number }
+* */
+const money = (options = {}) => {
+  const { currency, value } = options;
+  const locale = window.navigator.language;
+  const defaultCurrency = 'TRY';
+  const validCurrency = currency || defaultCurrency;
+
+  return new Intl
+    .NumberFormat(locale, { style: 'currency', currency: validCurrency }).format(value);
+};
+
 export {
   getLocaleFromPath,
   isLocaleValid,
+  money,
 };

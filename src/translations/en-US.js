c4b8c5d4cc30d6a9a042d36e5f8b92a144312284
İbrahim Turan <ibrahimturan002@gmail.com>
2021-07-08 23:06:33 +0300

    add new translations

--- src/translations/en-US.js
+++ src/translations/en-US.js
@@ -2,4 +2,6 @@ export default {
   'fourOFour.error.title': 'Error 404',
   'fourOFour.error.description': 'Maybe, this page was never created? We are just in simulation managed by aliens?',
   'website.title': 'Vivense Shop',
+  'out.of.stock': 'Out of stock',
+  products: 'Products',
 };
--- src/translations/tr-TR.js
+++ src/translations/tr-TR.js
@@ -2,4 +2,6 @@ export default {
   'fourOFour.error.title': '404 Hatası!',
   'fourOFour.error.description': 'Belkide bu safya hiç oluşmadı? Ya sadece uzaylılar tarafından yönetilen bir simülasyondaysak?',
   'website.title': 'Vivense Mağazası',
+  'out.of.stock': 'Tükendi',
+  products: 'Ürünler',
 };

a334d6a502e9a568b70344b9dfee89a3006fb672
İbrahim Turan <ibrahimturan002@gmail.com>
2021-07-08 23:06:58 +0300

    product list moved to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,46 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import productClass from '@/data/products';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    products: [],
+    page: 1,
+  },
+  getters: {
+    products(state) {
+      return state.products;
+    },
+    page(state) {
+      return state.page;
+    },
   },
   mutations: {
+    setProducts(state, payload) {
+      state.products = payload;
+    },
+    addNewProducts(state, payload) {
+      state.products = [...state.products, ...payload];
+    },
+    setPage(state, newPage) {
+      state.page = newPage;
+    },
+    resetProducts(state) {
+      state.products = [];
+      state.page = 1;
+    },
   },
   actions: {
+    async fetchProductList({ commit }, options) {
+      const { initial = true, size = 10, page = 1 } = options;
+      const mutation = initial ? 'setProducts' : 'addProducts';
+      const result = await productClass.productList(size);
+
+      commit(mutation, result);
+      commit('setPage', page);
+    },
   },
   modules: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,185 +1,37 @@
 <script>
+import { mapActions, mapGetters } from 'vuex';
 import VProductCard from '@/components/VProductCard';
 
 export default {
   name: 'Home',
   components: { VProductCard },
-  data() {
-    return {
-      products: [
-        {
-          id: 0,
-          title: 'Nunc sodales dignissim nibh at lacinia',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: this.getRandomRatingAmount(),
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-        {
-          id: 1,
-          title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: this.getRandomRatingAmount(),
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-        {
-          id: 2,
-          title: 'Pellentesque condimentum est vel diam lobortis',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: 0,
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-          specialPrice: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-        {
-          id: 3,
-          title: 'Vel fermentum massa molestie. Proin tempus metus a pretium mattis',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: this.getRandomRatingAmount(),
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-        {
-          id: 4,
-          title: 'Nulla venenatis vestibulum sit amet sit amet leo',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: this.getRandomRatingAmount(),
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-        {
-          id: 5,
-          title: 'Praesent eu ex suscipit, posuere purus nec',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: this.getRandomRatingAmount(),
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-          specialPrice: { value: 8, currency: 'TRY' },
-        },
-        {
-          id: 6,
-          title: 'Pellentesque id orci nibh',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: this.getRandomRatingAmount(),
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-        {
-          id: 7,
-          title: 'Aliquam non magna nec elit commodo vestibulum',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: 12,
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-        {
-          id: 8,
-          title: 'Pellentesque quis mi nec turpis euismod',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: this.getRandomRatingAmount(),
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-        {
-          id: 9,
-          title: 'Quis aliquet purus nulla id ligula',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: 0,
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-          specialPrice: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-        {
-          id: 10,
-          title: 'Nulla ut viverra ante, in posuere eros',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: this.getRandomRatingAmount(),
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-        {
-          id: 11,
-          title: 'Curabitur eget gravida turpis',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: this.getRandomRatingAmount(),
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-        {
-          id: 12,
-          title: 'Sed tristique commodo tortor a aliquam',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: this.getRandomRatingAmount(),
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-        {
-          id: 13,
-          title: 'Nunc ut nisl dictum augue congue',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: this.getRandomRatingAmount(),
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-          specialPrice: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-        {
-          id: 14,
-          title: 'Curabitur iaculis condimentum ipsum,',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: this.getRandomRatingAmount(),
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-          specialPrice: { value: 43, currency: 'TRY' },
-        },
-        {
-          id: 15,
-          title: 'Etiam ac tellus sed arcu mollis gravida ac placerat lorem',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: 0,
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-        {
-          id: 16,
-          title: 'Integer tempus magna nisi',
-          image: 'https://picsum.photos/310/200',
-          ratingAmount: this.getRandomRatingAmount(),
-          averageRating: this.getRandomRating(),
-          stock: this.getRandomRatingAmount(),
-          price: { value: this.getRandomPrice(), currency: 'TRY' },
-        },
-      ],
-    };
+  computed: {
+    ...mapGetters({
+      products: 'products',
+      page: 'page',
+    }),
+    isMobile() {
+      return this.$vuetify.breakpoint.smAndDown;
+    },
+  },
+  async created() {
+    await this.fetchProductList({
+      size: this.isMobile ? 5 : 8,
+      initial: true,
+      page: this.getPageFromQuery(),
+    });
   },
   methods: {
+    ...mapActions({
+      fetchProductList: 'fetchProductList',
+    }),
     getStockLabel(product) {
-      return !product.stock && 'Out of Stock';
+      return !product.stock && this.$t('out.of.stock');
     },
-    getRandomRating() {
-      return (Math.random() * 10) % 5;
-    },
-    getRandomRatingAmount() {
-      return ((Math.random() * 10000) % 250).toFixed(0);
-    },
-    getRandomPrice() {
-      return Math.random() * 100;
+    getPageFromQuery() {
+      const { page = 1 } = this.$route.query;
+
+      return page;
     },
   },
 };
@@ -189,7 +41,7 @@ export default {
   <v-container class="home-page py-10">
     <v-row>
       <v-col cols="auto">
-        <h1 class="display-2">Products</h1>
+        <h1 class="display-2">{{ $t('products') }}</h1>
       </v-col>
     </v-row>
 

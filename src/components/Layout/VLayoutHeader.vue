da8f9d519b6bae15a18883622175980f9e32fd85
İbrahim Turan <ibrahimturan002@gmail.com>
2021-07-08 21:01:19 +0300

    add language switching

--- src/components/Layout/VLayoutHeader.vue
+++ src/components/Layout/VLayoutHeader.vue
@@ -1,9 +1,37 @@
 <script>
 export default {
   name: 'VLayoutHeader',
+  computed: {
+    locales() {
+      return {
+        tr: 'Türkçe',
+        en: 'English',
+      };
+    },
+    currentLocale() {
+      return this.$i18n.locale;
+    },
+    otherLocaleKey() {
+      return Object.keys(this.locales).find((key) => key !== this.currentLocale);
+    },
+    otherLocale() {
+      return this.locales[this.otherLocaleKey];
+    },
+  },
   methods: {
     redirectHome() {
       this.$router.push({ name: 'Home' });
+    },
+    changeLocale() {
+      this.$router.push({
+        ...this.$route,
+        params: {
+          ...this.$route.params,
+          language: this.otherLocaleKey,
+        },
+      });
+
+      this.$i18n.locale = this.otherLocaleKey;
     },
   },
 };
@@ -23,6 +51,14 @@ export default {
       <h1 class="display-1">
         {{ $t('website.title') }}
       </h1>
+    </v-btn>
+
+    <v-spacer />
+
+    <v-btn text @click="changeLocale">
+      <v-icon>mdi mdi-translate</v-icon>
+
+      {{ otherLocale }}
     </v-btn>
   </v-app-bar>
 </template>

57d10cfed91d3c27c35eb80e7e0d93bca0340bc7
İbrahim Turan <ibrahimturan002@gmail.com>
2021-07-09 23:08:44 +0300

    add infinite scroll package

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import i18n from './plugins/i18n';
+import './plugins/infiniteScroll';
 
 Vue.config.productionTip = false;
 

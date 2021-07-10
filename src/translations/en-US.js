094073a27afa6fa5e541d4750e79ba4d9b7574b0
İbrahim Turan <ibrahimturan002@gmail.com>
2021-07-10 17:21:18 +0300

    add missing translations

--- src/translations/en-US.js
+++ src/translations/en-US.js
@@ -3,5 +3,9 @@ export default {
   'fourOFour.error.description': 'Maybe, this page was never created? We are just in simulation managed by aliens?',
   'website.title': 'Vivense Shop',
   'out.of.stock': 'Out of stock',
+  'in.stock': 'In stock',
   products: 'Products',
+  description: 'Description',
+  'add.to.cart': 'Add to cart',
+  'out.of.stock.message': 'This product couldn\'t have supplied yet.',
 };
--- src/translations/tr-TR.js
+++ src/translations/tr-TR.js
@@ -3,5 +3,9 @@ export default {
   'fourOFour.error.description': 'Belkide bu safya hiç oluşmadı? Ya sadece uzaylılar tarafından yönetilen bir simülasyondaysak?',
   'website.title': 'Vivense Mağazası',
   'out.of.stock': 'Tükendi',
+  'in.stock': 'Stokta',
   products: 'Ürünler',
+  description: 'Açıklama',
+  'add.to.cart': 'Sepete ekle',
+  'out.of.stock.message': 'Bu ürün şuan tedarik edilememektedir.',
 };

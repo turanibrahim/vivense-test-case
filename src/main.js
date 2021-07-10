6ae50ca82030c62c2e5a140cfd2ac084c86c6289
İbrahim Turan <ibrahimturan002@gmail.com>
2021-07-10 17:20:04 +0300

    add slider to project

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store';
 import vuetify from './plugins/vuetify';
 import i18n from './plugins/i18n';
 import './plugins/infiniteScroll';
+import './plugins/awesomeSwiper';
 
 Vue.config.productionTip = false;
 
